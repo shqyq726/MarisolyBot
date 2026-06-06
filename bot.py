9092f668aefb9605967e0f9a0ab403d26fe96402
shqyq726 <shaghayeghar05@gmail.com>
2026-06-06 13:39:14 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,16 @@
 from flask import Flask, request
-from telegram import Bot, Update
-from telegram.ext import Dispatcher, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram import Update, Bot
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 import os
 
 TOKEN = "8681244479:AAHqAg2hYNu8HHHJ5NgWbcqrZQmDY77a2KI"
 ADMIN_ID = 777430200
-URL = "https://marisolybot.onrender.com"
+BASE_URL = "https://marisolybot.onrender.com"
 
 app = Flask(__name__)
-bot = Bot(token=TOKEN)
 
-# Dispatcher
-dispatcher = Dispatcher(bot=bot, update_queue=None)
+bot = Bot(token=TOKEN)
+application = Application.builder().token(TOKEN).build()
 
 # /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -19,23 +18,23 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # پیام ناشناس
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_text = update.message.text
+    text = update.message.text
 
     await bot.send_message(
         chat_id=ADMIN_ID,
-        text=f"📩 پیام ناشناس:\n\n{user_text}"
+        text=f"📩 پیام ناشناس:\n\n{text}"
     )
 
     await update.message.reply_text("✅ ارسال شد!")
 
-dispatcher.add_handler(CommandHandler("start", start))
-dispatcher.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+application.add_handler(CommandHandler("start", start))
+application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-# Webhook route
-@app.route(f"/{TOKEN}", methods=["POST"])
+# webhook route
+@app.route(f"/webhook", methods=["POST"])
 def webhook():
     update = Update.de_json(request.get_json(force=True), bot)
-    dispatcher.process_update(update)
+    application.update_queue.put_nowait(update)
     return "ok"
 
 @app.route("/")
@@ -45,7 +44,7 @@ def home():
 # set webhook
 @app.route("/setwebhook")
 def set_webhook():
-    bot.set_webhook(url=f"{URL}/{TOKEN}")
+    bot.set_webhook(url=f"{BASE_URL}/webhook")
     return "Webhook set!"
 
 if __name__ == "__main__":
