a6b663da655cfb3ce9955fa3a085b085ca4f5874
shqyq726 <shaghayeghar05@gmail.com>
2026-06-06 15:22:13 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 from telegram import Bot, Update
-import requests
 
 TOKEN = "8681244479:AAHqAg2hYNu8HHHJ5NgWbcqrZQmDY77a2KI"
 ADMIN_ID = 777430200
@@ -8,9 +7,6 @@ URL = "https://marisolybot.onrender.com/webhook"
 
 app = Flask(__name__)
 bot = Bot(token=TOKEN)
-
-def set_webhook():
-    requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook", params={"url": URL})
 
 
 @app.route("/")
@@ -22,26 +18,24 @@ def home():
 def webhook():
     data = request.get_json()
 
-    if not data:
-        return "no data"
-
     update = Update.de_json(data, bot)
 
-    if update.message and update.message.text:
-
+    if update.message:
         text = update.message.text
+        chat_id = update.message.chat.id
 
         if text == "/start":
-            bot.send_message(chat_id=update.message.chat_id,
-                             text="🌊 خوش اومدی به Marisol")
-        else:
-            bot.send_message(chat_id=ADMIN_ID,
-                             text=f"📩 پیام:\n{text}")
+            bot.send_message(chat_id=chat_id, text="🌊 خوش اومدی به Marisol")
 
-            bot.send_message(chat_id=update.message.chat_id,
-                             text="ارسال شد 🌊")
+        else:
+            bot.send_message(chat_id=ADMIN_ID, text=f"📩 پیام:\n{text}")
+            bot.send_message(chat_id=chat_id, text="ارسال شد 🌊")
 
     return "ok"
+
+
+def set_webhook():
+    bot.set_webhook(URL)
 
 
 if __name__ == "__main__":
